ad399979a59fc912df5ce680e18920ee06f0b217
manager <manager@163.com>
2018-06-19 19:57:26 +0800

    项目基本配置

--- manage.py
+++ manage.py
@@ -1,7 +1,40 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import redis
+from flask_wtf import CSRFProtect
+from flask_session import Session
+from flask_script import Manager
+from flask_migrate import migrate, MigrateCommand
 
 app = Flask(__name__)
 
+
+class Config(object):
+    """工程配置信息"""
+    DEBUG = True
+    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
+    # 数据库的配置信息
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/myblog"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # redis配置
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
+    # flask_session的配置信息
+    SESSION_TYPE = "redis"  # 指定 session 保存到 redis 中
+    SESSION_USE_SIGNER = True  # 让 cookie 中的 session_id 被加密签名处理
+    SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # 使用 redis 的实例
+    PERMANENT_SESSION_LIFETIME = 86400  # session 的有效期，单位是秒
+
+
+
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+CSRFProtect(app)
+Session(app)
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 @app.route('/')
 def hello_world():
@@ -9,4 +42,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
